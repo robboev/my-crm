7cbac5b0fab166ada335098ba4cd9a17fc48c931
robboev <robert100@gmail.com>
2025-04-04 09:05:50 +0300

    Remove sensitive files from repository

--- crm.sql
+++
@@ -1,9 +0,0 @@
-
-CREATE TABLE `leads` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `phone` varchar(50) NOT NULL,
-  `status` varchar(50) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
